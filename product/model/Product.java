d35216c9712baff0299f4a73713819b7bf8c61c0
puvithk <puvithkumar2004@gmail.com>
2026-05-08 22:49:30 +0530

    Designing product module

---
+++ product/model/Product.java
@@ -0,0 +1,11 @@
+package product.model;
+
+public class Product {
+    private String productTitle;
+    // Food product
+    // Key information
+    // Nutrition info
+   // In Food prodcuts // info -> Description , unit , FSSAI License , shelf life , taste profile , Disclaimer ,  Custoner care details , country of origin , manufacture name and addtress , Marketers name and address , return policy , sugar profile
+    // In electroince - description , service cenetrd etails , discalimer , cuontry of origin , maunufature name and address , return policy  , unit , wattages , key fratures
+    // Create a base product class
+}
